bd4a1c88b1d8133c11fc1158049df8894c01e2d7
hknarra <51864119+hknarra@users.noreply.github.com>
2021-04-15 16:40:52 +0530

    code changed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Git practice project
 # USe below link, when prompted for URL
-# https://github.com/hknarra/My_python_project_1.git
+# https://github.com/hknarra/Project_Git
 
 print('this is 1st line of project form pycharm into Git')
